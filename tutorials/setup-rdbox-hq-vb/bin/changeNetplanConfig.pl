e0549febc0fd0f184c761e5ae10f62135d21646a
ishida-shigeru-intec <ishida_shigeru@intec.co.jp>
2019-06-17 11:31:16 +0900

    The VMs construction on Virtualbox/Vagrant is now compatible with Ubuntu 18.04.

---
+++ tutorials/setup-rdbox-hq-vb/bin/changeNetplanConfig.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
+
+my $nic_for_del_gw = "";
+my $nic_for_add_gw = "";
+my $gw = "";
+
+GetOptions(
+  'd|i=s' => \$nic_for_del_gw,
+  'a|i=s' => \$nic_for_add_gw,
+  'r|i=s' => \$gw
+  );
+
+($nic_for_add_gw ne "" && $gw eq "") && die "IP address for default-route on interface \"$nic_for_add_gw\" is not set.\n";
+
+while ($line = <>) {
+  if ($nic_for_del_gw ne "" && $line =~ /^\s+$nic_for_del_gw:$/) {
+    my $pos = index($line, $nic_for_del_gw);
+    my $indent_size = $pos / 2;
+    my $indent = "";
+
+    for ($i = 0; $i < 3 * $indent_size; $i++) {
+      $indent .= " ";
+    }
+    print $line;
+    print $indent . "dhcp4-overrides:\n";
+
+    $indent = "";
+    for ($i = 0; $i < 4 * $indent_size; $i++) {
+      $indent .= " ";
+    }
+    print $indent . "use-routes: false\n";
+  } elsif ($nic_for_add_gw ne "" && $line =~ /^\s+$nic_for_add_gw:$/) {
+    my $pos = index($line, $nic_for_add_gw);
+    my $indent_size = $pos / 2;
+    my $indent = "";
+
+    for ($i = 0; $i < 3 * $indent_size; $i++) {
+      $indent .= " ";
+    }
+    print $line;
+    print $indent . "routes:\n";
+    print $indent . "- to: 0.0.0.0/0\n";
+
+    $indent = "";
+    for ($i = 0; $i < 4 * $indent_size; $i++) {
+      $indent .= " ";
+    }
+    print $indent . "via: " . $gw . "\n";
+  } else {
+    print $line;
+  }
+}
