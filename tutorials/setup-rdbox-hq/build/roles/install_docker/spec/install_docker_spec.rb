b7ebba9006773c43cf1888bc253db103997b19f5
Toshio HIGUCHI <higuchi_toshio@intec.co.jp>
2019-11-12 06:44:47 +0000

    spec with environment variables

--- tutorials/setup-rdbox-hq/build/roles/install_docker/spec/install_docker_spec.rb
+++ tutorials/setup-rdbox-hq/build/roles/install_docker/spec/install_docker_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 #
 describe package("docker-ce") do
-  it { should be_installed.with_version("5:18.09.1~3-0~ubuntu-xenial") }
+  it { should be_installed.with_version(ENV["DOCKER_VERSION"]) }
 end      
 
 #
--- tutorials/setup-rdbox-hq/build/roles/install_kubernetes/spec/install_kubernetes_spec.rb
+++ tutorials/setup-rdbox-hq/build/roles/install_kubernetes/spec/install_kubernetes_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 #
 describe package("kubeadm") do
-  it { should be_installed.with_version("1.13.2-00") }
+  it { should be_installed.with_version(ENV["KUBEADM_VERSION"]) }
 end      
 
 #
 describe package("kubelet") do
-  it { should be_installed.with_version("1.13.2-00") }
+  it { should be_installed.with_version(ENV["KUBELET_VERSION"]) }
 end      
 
 #
 describe package("kubectl") do
-  it { should be_installed.with_version("1.13.2-00") }
+  it { should be_installed.with_version(ENV["KUBECTL_VERSION"]) }
 end      
 
 #
 describe package("kubernetes-cni") do
-  it { should be_installed.with_version("0.6.0-00") }
+  it { should be_installed.with_version(ENV["KUBECNI_VERSION"]) }
 end      
 
 #
